a81476a1c6e5326d6304ff5aaec212d132e73c37
Wenjie Chen <wjchen84@gmail.com>
2015-04-03 15:29:21 +0900

    change the folder path

--- example_pipeline/overhand.py
+++ example_pipeline/overhand.py
@@ -1,6 +1,6 @@
 import os
 from rapprentice.call_and_print import call_and_print
 os.chdir("../scripts")
-od="~/henry_sandbox/rapprentice/sampledata/overhand"
+od="~/catkin_ws/src/rapprentice/sampledata/overhand"
 call_and_print("./generate_annotations.py %(od)s/demo0_2013-04-20-15-58-09.bag %(od)s/demo0_2013-04-20-15-58-09.bag.ann.yaml"%dict(od=od))
 call_and_print("./generate_h5.py %(od)s/overhand.yaml"%dict(od=od))
